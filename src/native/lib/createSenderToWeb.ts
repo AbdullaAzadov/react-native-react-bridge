dafd3c2e19110c0c6767591726b099d558dfff7d
Abdulla <abdulla200532@icloud.com>
2025-06-28 12:27:17 +0500

    1.0.2: createSenderToWeb fixed

--- src/native/lib/createSenderToWeb.ts
+++ src/native/lib/createSenderToWeb.ts
@@ -1,25 +1,22 @@
-'use client';
-import type { RefObject } from 'react';
-import type { WebView } from 'react-native-webview';
-import type { BridgeMessage } from '../../types/bridge';
+import { RefObject } from 'react';
+import WebView from 'react-native-webview';
+import { BridgeMessage } from 'src/types/bridge';
 
 export function createSenderToWeb<TMap>() {
-  return {
-    send<K extends Extract<keyof TMap, string>>(
-      ref: RefObject<WebView>,
-      query: K,
-      data: TMap[K]
-    ) {
-      if (!ref.current) {
-        console.warn('[sendMessageToWeb] WebView ref is not attached');
-        return;
-      }
+  return function send<K extends Extract<keyof TMap, string>>(
+    ref: RefObject<WebView>,
+    query: K,
+    data: TMap[K]
+  ) {
+    if (!ref.current) {
+      console.warn('[sendMessageToWeb] WebView ref is not attached');
+      return;
+    }
 
-      const message: BridgeMessage<K, TMap[K]> = { query, data };
-      const payload = JSON.stringify(JSON.stringify(message));
-      const script = `window.dispatchEvent(new MessageEvent('message', { data: ${payload} }));`;
+    const message: BridgeMessage<K, TMap[K]> = { query, data };
+    const payload = JSON.stringify(JSON.stringify(message));
+    const script = `window.dispatchEvent(new MessageEvent('message', { data: ${payload} }));`;
 
-      ref.current.injectJavaScript(script);
-    },
+    ref.current.injectJavaScript(script);
   };
 }
